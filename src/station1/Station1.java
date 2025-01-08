a334136e60e677079b088ce95055944ff821a0f2
KO1231 <k.oka@kazutech.jp>
2025-01-08 21:44:17 +0900

    add: 01

--- src/station1/Station1.java
+++ src/station1/Station1.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Station1 {
 
@@ -9,11 +10,16 @@ public class Station1 {
 
     public static ArrayList<String> q1() {
         // 問題1: ここから
+        return new ArrayList<>(Arrays.asList("Tech", "Train"));
         // 問題1: ここまで
     }
 
     public static int q2(ArrayList<String> list, ArrayList<String> searchWords) {
         // 問題2: ここから
+        return searchWords.stream()
+            .filter(word -> list.contains(word))
+            .toList()
+            .size();
         // 問題2: ここまで
     }
 }
