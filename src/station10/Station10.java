420aa34b6f3a13a9445e915774866aaffc617976
KO1231 <k.oka@kazutech.jp>
2025-01-08 23:30:29 +0900

    add: 10

--- src/station10/Station10.java
+++ src/station10/Station10.java
@@ -5,6 +5,18 @@ public class Station10 {
 
     public static void q1(ArrayList<Integer> list) {
         // ここから
+        final ArrayList<String> formatted = new ArrayList<String>();
+        int maxSize = -1;
+        for (int i : list) {
+            String f = "%,d".formatted(i);
+            formatted.add(f);
+            if (f.length() > maxSize) {
+                maxSize = f.length();
+            }
+        }
+
+        final String format = "%" + maxSize + "s";
+        formatted.forEach(f -> System.out.println(String.format(format, f)));
         // ここまで
     }
 }
