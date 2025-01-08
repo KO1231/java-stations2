3611743e43bc51b702d5771e19b10d50dbd431d6
KO1231 <k.oka@kazutech.jp>
2025-01-08 23:12:30 +0900

    add: 08

--- src/station8/Station8.java
+++ src/station8/Station8.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.stream.Collectors;
 
 public class Station8 {
 
@@ -11,11 +12,21 @@ public class Station8 {
 
     public static HashSet<String> aggregateUserId(ArrayList<Order> orders) {
         // 問題1: ここから
+        return orders.stream()
+            .map(order -> order.userId)
+            .collect(Collectors.toCollection(HashSet::new));
         // 問題1: ここまで
     }
 
     public static HashMap<String, Integer> aggregateUserAndTotalPrice(ArrayList<Order> orders) {
         // 問題2: ここから
+        return new HashMap<>(
+            orders.stream()
+                .collect(Collectors.groupingBy(
+                    order -> order.userId, 
+                    Collectors.summingInt(order -> order.totalPrice)
+                ))
+        );
         // 問題2: ここまで
     }
 }
